f1b4e3a16012aa8449de46d626c7a8f0e543b14c
Alexoat76 <3915@holbertonschool.com>
2022-02-14 11:15:31 -0500

    Update header file

--- sort.h
+++ sort.h
@@ -34,6 +34,7 @@ void print_list(const listint_t *list);
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
+listint_t *get_head(listint_t *tmp);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
